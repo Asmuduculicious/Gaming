841720cb9d11dc62ebbdd9930acb0d5b3d02ec36
Asmuduculicious <hfu2@students.mags.school.nz>
2024-06-06 20:05:24 +1200

    readd

--- Actual Project/script/character.gd
+++ Actual Project/script/character.gd
@@ -3,12 +3,11 @@ extends CharacterBody2D
 @export var speed := 100.0
 @export var jump_velocity = -200.0
 @export var bullet_scene: PackedScene
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var double_jump = true
-var can_shoot = true
-
+var can_shoot_bow = true
+var can_shoot_gun = true
 var weapon = 0
 var weapon_idle = ["spear_idle", "bow_idle", "gun_idle"]
 var weapon_attack = ["spear_attack", "bow_attack", "gun_attack"]
@@ -17,22 +16,19 @@ func _ready():
 	$AnimatedSprite2D2.play("spear_idle")
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
 		double_jump = true
-			
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
 	elif Input.is_action_just_pressed("ui_accept") and double_jump:
 		velocity.y = jump_velocity
 		double_jump = false
+	
+	move_and_slide()
 
-	# Get the input direction and handle the movemednt/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * speed
@@ -42,39 +38,45 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 	
-	
-	if Input.is_action_just_pressed("ui_swap"):
-		weapon += 1
-		if weapon == 3:
-			weapon = 0
+	if Input.is_action_just_pressed("ui_spear"):
+		weapon = 0
+		$AnimatedSprite2D2.play(weapon_idle[weapon])
+	elif Input.is_action_just_pressed("ui_bow"):
+		weapon = 1
+		$AnimatedSprite2D2.play(weapon_idle[weapon])
+	elif Input.is_action_just_pressed("ui_gun"):
+		weapon = 2
 		$AnimatedSprite2D2.play(weapon_idle[weapon])
 	
-	if Input.is_action_just_pressed("ui_attack"):
-		if weapon == 1 and can_shoot:
-			$Node2D/Timer2.start()
-			$AnimatedSprite2D2.play(weapon_attack[weapon])
-			can_shoot = false
-		elif weapon != 1:
-			$AnimatedSprite2D2.play(weapon_attack[weapon])
-		
-
-
-	move_and_slide()
 	
-				
+	if Input.is_action_just_pressed("ui_attack"):
+		if weapon == 0: 
+			$AnimatedSprite2D2.play(weapon_attack[weapon])
+		elif weapon == 1 and can_shoot_bow:
+			$Node2D/Timer.start()
+			$AnimatedSprite2D2.play(weapon_attack[weapon])
+			can_shoot_bow = false
+		elif weapon == 2 and can_shoot_gun:
+			$Node2D/Timer3.start()
+			$AnimatedSprite2D2.play(weapon_attack[weapon])
+			can_shoot_gun = false
+			
 func _shoot():
 	var bullet = bullet_scene.instantiate()
-	bullet.position = $AnimatedSprite2D/arrow_spawn.global_position
+	bullet.position = $arrow_spawn.global_position
 	bullet.direction = $AnimatedSprite2D.scale.x
 	add_sibling(bullet)
-
+	
 func _on_timer_timeout():
-	can_shoot = true
-		
+	_shoot()
+	$Node2D/Timer2.start()
 
 func _on_timer_2_timeout():
+	can_shoot_bow = true
+		
+func _on_timer_3_timeout():
 	_shoot()
-	can_shoot = false
-	$Node2D/Timer.start()
+	$Node2D/Timer4.start()
 
-
+func _on_timer_4_timeout():
+	can_shoot_gun= true
